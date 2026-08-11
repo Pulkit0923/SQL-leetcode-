8106feef74b9c52e2717439e62e84c3d09a2eeb6
Pulkit0923 <2k23.csiot2311109@gmail.com>
2026-08-11 14:56:13 +0530

    Time: 532 ms (42.85%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -4,4 +4,5 @@ name
 FROM
 Customer
 WHERE
-referee_id != 2 OR referee_id is NULL
+referee_id != 2 OR referee_id is NULL 
+# sql do not retuen null vlaues by default so if we wnt it so we need to mention it explicitely
